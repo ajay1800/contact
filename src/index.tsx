5440bce8107e22441dd4fe26840bb64d999c1ff3
ajay <ajay@unifiedinfotech.net>
2023-05-26 09:29:00 +0530

    url changes

--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename='/contact'>
+      <BrowserRouter basename='/contact/contact'>
         <QueryClientProvider client={queryClient}>
           <App />
         </QueryClientProvider>
